79da4fca9bab9df94610b9af5c2ccb8483a9eabb
Arcelyth <arcelyth@tutamail.com>
2026-02-21 11:57:42 +0800

    add build_ast

--- erlisp.erl
+++ erlisp.erl
@@ -11,6 +11,23 @@
     | {quote, lobject()} 
     | {closure, [string()], any(), env()}.
 
+-type exp() :: {literal, lobject()}
+    | {var, string()}
+    | {if_, exp(), exp(), exp()}
+    | {and_, exp(), exp()}
+    | {or_, exp(), exp()}
+    | {apply, exp(), exp()}
+    | {call, exp(), [exp()]}
+    | {lambda, [string()], exp()}
+    | {let_, let_kind(), [{string(), exp()}], exp()}
+    | {defexp, def()}.
+
+-type let_kind() :: let_ | letstar | letrec.
+
+-type def() :: {val, string(), exp()}
+    | {def, string(), [string()], exp()}
+    | {exp, exp()}.
+
 -record(stream, {
     line_num = 1,
     chars = [] :: [char()],
@@ -35,7 +52,7 @@ read_char(Stm = #stream{chars = [C | Rest]}) ->
         _ -> Stm#stream{chars = Rest}
     end,
     {C, NewStm};
-read_char(Stm = #stream{chars = [], chan = Io}) -> 
+read_char(Stm = #stream{chars = [], chan = {_, Io}}) -> 
     case io:get_chars(Io, "", 1) of
         eof -> {eof, Stm};
         {error, Reason} -> erlang:error({io_error, Reason});
@@ -67,7 +84,6 @@ handle_comment(Stm) ->
         {$\n, Stm1} -> Stm1;
         {_, Stm1} -> handle_comment(Stm1)
     end.
-
 
 read_symbol(Stm) ->
     read_symbol(Stm, []).
@@ -132,14 +148,85 @@ read_sexp(Stm) ->
             end
     end.
 
-build_ast(Sexp) ->
-    ok.
+cond_to_if([]) -> {literal, nil};
+cond_to_if([{pair, {symbol, "else"}, {pair, Exp, nil}} | _]) ->
+    build_ast(Exp);
+cond_to_if([{pair, Cond, {pair, Exp, nil}} | T]) ->
+    {if_, build_ast(Cond), build_ast(Exp), cond_to_if(T)};
+cond_to_if(_) ->
+    erlang:error({syntax_error, "Invalid cond syntax"}).
+
+lobject_to_list(nil) -> [];
+lobject_to_list({pair, Car, Cdr}) -> [Car | lobject_to_list(Cdr)];
+lobject_to_list(Other) -> erlang:error({type_error, "Not a proper list", Other}).
+
+is_lobject_list(nil) -> true;
+is_lobject_list({pair, _, Cdr}) -> is_lobject_list(Cdr);
+is_lobject_list(_) -> false.
+
+-spec build_ast(lobject()) -> exp().
+build_ast({fixnum, _} = Obj) -> {literal, Obj};
+build_ast({boolean, _} = Obj) -> {literal, Obj};
+build_ast(nil) -> {literal, nil};
+build_ast({quote, _} = Obj) -> {literal, Obj};
+build_ast({symbol, S}) -> {var, S};
+build_ast(Sexp = {pair, _, _}) -> 
+    case is_lobject_list(Sexp) of 
+        true -> 
+            List = lobject_to_list(Sexp),
+            case List of
+                [{symbol, "if"}, Cond, Iftrue, Iffalse] -> 
+                    {if_, build_ast(Cond), build_ast(Iftrue), build_ast(Iffalse)};
+                [{symbol, "and"}, C1, C2] -> 
+                    {and_, build_ast(C1), build_ast(C2)};
+                [{symbol, "or"}, C1, C2] -> 
+                    {or_, build_ast(C1), build_ast(C2)};
+                [{symbol, "quote"}, Sexp] -> 
+                    {literal, {quote, Sexp}};
+                [{symbol, "lambda"}, NamesObj, Body] ->
+                    case is_lobject_list(NamesObj) of
+                        true ->
+                            Names = [case S of {symbol, Name} -> Name; _ -> erlang:error({type_error, "Lambda formals must be symbols"}) end || S <- lobject_to_list(NamesObj)],
+                            {lambda, Names, build_ast(Body)};
+                        false -> erlang:error({type_error, "Lambda formals must be a list"})
+                    end;
+                [{symbol, "define"}, {symbol, N}, NamesObj, Body] ->
+                    Names = [case S of {symbol, Name} -> Name; _ -> erlang:error(type_error) end || S <- lobject_to_list(NamesObj)],
+                    {defexp, {def, N, Names, build_ast(Body)}};
+                [{symbol, "apply"}, Fnexp, Args] -> 
+                    {apply, build_ast(Fnexp), build_ast(Args)};
+                [{symbol, "cond"} | Conditions] -> 
+                    cond_to_if(Conditions);
+                [{symbol, S}, Bindings, Exp] when S =:= "let"; S =:= "let*"; S =:= "letrec" ->
+                    Kind = case S of "let" -> let_; "let*" -> letstar; "letrec" -> letrec end,
+                    ParsedBindings = [parse_binding(B) || B <- lobject_to_list(Bindings)],
+                    assert_unique([Name || {Name, _} <- ParsedBindings]),
+                    {let_, Kind, ParsedBindings, build_ast(Exp)};
+                [Fnexp | Args]  -> 
+                    {call, build_ast(Fnexp), [build_ast(S) || S <- Args]};
+                [] -> erlang:error({parse_error, "Poorly formed expression"})
+            end;
+        false -> {literal, Sexp}
+    end;
+build_ast({primitive, _, _} = _) -> 
+    erlang:error({syntax_error, "Primitive cannot appear in source"}).
+
+parse_binding({pair, {symbol, N}, {pair, E, nil}}) ->
+    {N, build_ast(E)};
+parse_binding(_) ->
+    erlang:error({type_error, "Let binding must be (name exp)"}).
+
+assert_unique(Names) ->
+    case length(Names) =:= length(lists:usort(Names)) of
+        true -> ok;
+        false -> erlang:error({type_error, "Duplicate names in let bindings"})
+    end.
 
 string_val(Val) ->
-    ok.
+    Val.
 
 eval(Ast, Env) ->
-    ok.
+    {Ast, 1}.
 
 repl(Stm, Env) ->
     repl(Stm, Env, element(1, Stm#stream.chan) =:= stdin).
@@ -150,13 +237,14 @@ repl(Stm, Env, false) ->
     do_repl(Stm, Env, false).
 
 do_repl(Stm, Env, IsStdin) ->
-    Ast = build_ast(read_sexp(Stm)),
+    {Sexp, Stm1} = read_sexp(Stm),
+    Ast = build_ast(Sexp),
     {Result, NewEnv} = eval(Ast, Env),
     case IsStdin of
-        true -> io:format("~s~n", [string_val(Result)]);
+        true -> io:format("~p~n", [string_val(Result)]);
         false -> ok
     end,
-    repl(Stm, NewEnv).
+    repl(Stm1, NewEnv).
 
 
 -spec get_ic() -> chan().
